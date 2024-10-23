d977d5d0d3b29438e450b4730b7b7371cbd216df
GGGMatt89 <matt.fntn89@gmail.com>
2024-10-23 14:28:39 +0200

    commit after lesson 5

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    @hasSection('title')
+        <title>@yield('title') - {{ config('app.name') }}</title>
+    @else
+        <title>{{ config('app.name') }}</title>
+    @endif
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <!-- Styles -->
+    <style>
+        [x-cloak] { display: none; }
+    </style>
+    @livewireStyles
+
+</head>
+<body>
+    @yield('body')
+
+    @livewireScripts
+</body>
+</html>
